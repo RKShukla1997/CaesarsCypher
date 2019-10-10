5f6ba11fa38ccf857778814d8294456c5fa0a4f0
Rudresh Kumar Shukla <rudreshkumarshukla9@gmail.com>
2019-10-10 22:33:12 +0530

    Third Commit

--- CaesarsCypher.java
+++ CaesarsCypher.java
@@ -45,16 +45,86 @@ public class CaesarsCypher{
 		return final_string;
 	}
 
-	public static void main(String[] args){
+	public static boolean check(File file_to_read,File file_to_write) throws Exception{
+		if(!file_to_write.exists()){
+			file_to_write.createNewFile();
+		}
+		else{
+			return true;
+		}
+
+		if(!file_to_read.exists()){
+			throw new Exception("Input file does not Exists");
+		}
+		else{
+			return true;
+		}
+	}
+
+	public static void encrypt_read_write(File file_to_read,File file_to_write,int n) throws Exception {
+		String encrypted =  "";
+		BufferedReader br = new BufferedReader(new FileReader(file_to_read));
+		String x;
+		FileWriter fw = new FileWriter(file_to_write);
+		while((x=br.readLine()) != null)
+			encrypted += CaesarsCypher.encrypt(x,n) + "\n";
+
+		fw.write(encrypted + "\n");
+		fw.close();
+
+	}
+
+	public static void decrypt_read_write(File file_to_read,File file_to_write,int n) throws Exception {
+		String decrypted =  "";
+		BufferedReader br = new BufferedReader(new FileReader(file_to_read));
+		String x;
+		FileWriter fw = new FileWriter(file_to_write);
+		while((x=br.readLine()) != null)
+			decrypted += CaesarsCypher.decrypt(x,n) + "\n";
+
+		fw.write(decrypted + "\n");
+		fw.close();
+
+	}
+
+	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the String to Encrypt: ");
-		String input_string = sc.nextLine();
-		System.out.println("Enter the number to shift: ");
-		int n = sc.nextInt();
-		String encrypted = CaesarsCypher.encrypt(input_string,n);
-		System.out.println(encrypted);
-		String decrypted = CaesarsCypher.decrypt(encrypted,n);
-		System.out.println(decrypted);
+		String inputFile = args[0];
+		String outputFile = args[1];
+		File file_to_read = new File(inputFile);
+		File file_to_write = new File(outputFile);
+		int n=1;
+
+		if(check(file_to_read,file_to_write)){
+            if(args.length == 2){
+                CaesarsCypher.encrypt_read_write(file_to_read,file_to_write,n);
+            }else if (args.length == 4) {
+            	n = Integer.parseInt(args[3]);
+                String operation = args[2];
+                if(operation.equals("encrypt")) {
+                    CaesarsCypher.encrypt_read_write(file_to_read,file_to_write,n);
+                }else{
+                    CaesarsCypher.decrypt_read_write(file_to_read,file_to_write,n);
+                }
+            }else if (args.length == 3) {
+            	String operation = args[2];
+                if(operation.equals("encrypt")) {
+                    CaesarsCypher.encrypt_read_write(file_to_read,file_to_write,n);
+                }else{
+                    CaesarsCypher.decrypt_read_write(file_to_read,file_to_write,n);
+                }
+       }
+
+    }
+
+		// System.out.println("Enter the String to Encrypt: ");
+		// String input_string = sc.nextLine();
+		// System.out.println("Enter the number to shift: ");
+		// int n = sc.nextInt();
+		// String encrypted = CaesarsCypher.encrypt(input_string,n);
+		// System.out.println(encrypted);
+		// String decrypted = CaesarsCypher.decrypt(encrypted,n);
+		// System.out.println(decrypted);
 
 	}
 } 
